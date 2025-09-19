bd01eaa6ef4529c38f5af6ec671abbc709df1761
anupgautam60 <anupgautam60@gmail.com>
2025-09-19 10:51:33 +0545

    cors problem solvef

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -3,23 +3,26 @@ import { allowedOrigins } from './allowedOrigins.js';
 const corsOptions = {
     origin: (origin, callback) => {
         const ngrokPattern = /https:\/\/[a-z0-9]+\.ngrok\.io/;
-        if (allowedOrigins.indexOf(origin) !== -1 || ngrokPattern.test(origin)) {
-            callback(null, true);  
+
+        // Allow requests with no origin (mobile apps, Postman, etc.)
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        // Normalize origin by removing trailing slashes
+        const normalizedOrigin = origin.replace(/\/$/, '');
+
+        if (allowedOrigins.includes(normalizedOrigin) || ngrokPattern.test(normalizedOrigin)) {
+            callback(null, true);
         } else {
-            callback(new Error('Not allowed by CORS'), false); 
+            console.error(`❌ CORS blocked: ${origin}`);
+            callback(new Error('Not allowed by CORS'));
         }
     },
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials:true,
+    credentials: true,
     optionsSuccessStatus: 200
 };
-
-// const corsOptions = {
-//     origin: ['https://nimtoz.com', 'https://www.nimtoz.com', 'http:localhost:3000'], // Allow your frontend domain
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
-//     allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-//     credentials: true, // Enable cookies/auth headers if required
-// };
 
 export default corsOptions;
