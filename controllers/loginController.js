2fd6e0fbe01c6f9dbb449f90309d18a3add9cca2
anupgautam60 <anupgautam60@gmail.com>
2025-09-19 10:58:59 +0545

    login changes

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -6,50 +6,41 @@ import { z } from 'zod'
 import { prisma } from '../config/prisma.js'
 
 
+//! Login User
 const loginUser = async (req, res) => {
-    let validatedData;
-    try {
-      validatedData = loginSchema.parse(req.body);
-    } catch (err) {
-      if (err instanceof z.ZodError) {
-        return res.status(400).json({ success: false, errors: err.errors });
-      }
-      return res.status(500).json({ success: false, message: 'Server error' });
-    }
-  
-    const { email, phone_number, password } = validatedData;
-  
-    // Find user by email OR phone
-    const foundUser = await prisma.user.findFirst({
-      where: {
-        OR: [
-          email ? { email } : {},
-          phone_number ? { phone_number } : {}
-        ]
-      }
-    });
-  
-    if (!foundUser) return res.status(404).json("User Not Found");
-  
+    const validatedData = loginSchema.parse(req.body)
+    const { email, password } = validatedData;
+
+    const foundUser = await prisma.user.findUnique({ where: { email } })
+
+    if (!foundUser) return res.status(404).json("User Not Found")
+    // if (!foundUser.isVerified) return res.status(400).json('Please verify your account first');
+
     const validPassword = bcrypt.compareSync(password, foundUser.password);
-    if (!validPassword) return res.status(401).json('Invalid credentials');
-  
+
+    if (!validPassword) return res.status(404).json('Invalid password');
+
     const accessToken = generateAccesstoken(foundUser);
     const refreshToken = generateRefreshToken(foundUser);
-  
-    const updatedUser = await prisma.user.update({
-      where: { id: foundUser.id },
-      data: { refreshToken }
-    });
-  
-    return res.json({
-      success: true,
-      accessToken,
-      refreshToken,
-      user: updatedUser
-    });
-  };
-  
+
+    try {
+        const updatedUser = await prisma.user.update({
+            where: { id: foundUser.id },
+            data: { refreshToken }
+        });
+
+        return res.json({ accessToken, refreshToken, user: updatedUser })
+
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            return res.status(400).json({
+                success: false,
+                errors: error.errors.map((e) => e.message)
+            });
+        }
+        res.status(400).json({ success: false, message: error.message });
+    }
+}
 
 export {
     loginUser
