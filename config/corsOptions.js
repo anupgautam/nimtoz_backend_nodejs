10b05837eb1500011e685d3ad64feecdbf5b23c4
anupgautam60 <anupgautam60@gmail.com>
2025-09-25 12:53:29 +0545

    expriry time increased

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,24 +1,5 @@
-import { allowedOrigins } from './allowedOrigins.js';
-
 const corsOptions = {
-    origin: (origin, callback) => {
-        const ngrokPattern = /https:\/\/[a-z0-9]+\.ngrok\.io/;
-
-        // Allow requests with no origin (mobile apps, Postman, etc.)
-        if (!origin) {
-            return callback(null, true);
-        }
-
-        // Normalize origin by removing trailing slashes
-        const normalizedOrigin = origin.replace(/\/$/, '');
-
-        if (allowedOrigins.includes(normalizedOrigin) || ngrokPattern.test(normalizedOrigin)) {
-            callback(null, true);
-        } else {
-            console.error(`❌ CORS blocked: ${origin}`);
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: '*', // allow all origins
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
