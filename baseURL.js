1d7d5a1e3828ba7928338d34fc4579b61b4592ef
anupgautam60 <anupgautam60@gmail.com>
2025-12-17 12:02:24 +0545

    images changes

--- baseURL.js
+++ baseURL.js
@@ -1,6 +1,6 @@
 // export const BASE_URL = "http://192.168.1.222:1000"
 
-export const BASE_URL = "https://nimtoz.thinfortech.com/"
+export const BASE_URL = "https://nimtoz.dhangadhiskincare.com/"
 
 // export const BASE_URL = "https://preoccipital-dismayingly-magnus.ngrok-free.dev"
 // export const BASE_URL = "https://www.nimtoz.com"
